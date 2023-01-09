ff3c1de3db92c6d7d13d6a48958f7c0c1b9b885e
adampl2 <adamchwiulkowski@gmail.com>
2023-01-09 02:35:13 +0000

    css update

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,7 +15,32 @@ function userInformationHTML(user) {
     </div>`
 }
 
+function repoInformationHTML(repos) {
+    if (repos.lenght === 0) {
+        return `<div class="clearfix repo-list">No repositories found.</div>`
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`
+    });
+
+    return `
+    <div class="clearfix repo-list">
+    <p>
+    <strong>Repo List:</strong>
+    </p>
+    <ul>
+    ${listItemsHTML.join('\n')}
+    </ul>
+    </div>`;
+}
+
 function fetchGitHubInformation(event) {
+
+    $('#gh-user-data').html("");
+    $('#gh-repo-data').html("");
 
     var username = $('#gh-username').val();
     if (!username) {
@@ -29,15 +54,21 @@ function fetchGitHubInformation(event) {
     )
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function (response) {
-            var userData = response;
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repos = secondResponse[0];
             $('#gh-user-data').html(userInformationHTML(userData));
+            $('#gh-repo-data').html(repoInformationHTML(repos));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
                 $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $('#gh-user-data').html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
                 console.log(errorResponse);
                 $('#gh-user-data').html(
@@ -46,3 +77,5 @@ function fetchGitHubInformation(event) {
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation);
