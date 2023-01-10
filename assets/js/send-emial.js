5c42ff8f87d9db560c2044b86a6bada2da3d19cb
adampl2 <adamchwiulkowski@gmail.com>
2023-01-10 01:32:45 +0000

    email configuration added.

---
+++ assets/js/send-emial.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_qsv99lb", "template_os5sdfw", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response.status, response.text);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
+}
